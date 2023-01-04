502bc20796f2c094ac88e07a98779b1a4522259c
NoureddineHelb <nbozanih@helb-prigogine.be>
2023-01-04 20:41:21 +0100

    event calendar 4

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -10,8 +10,8 @@ class SaleOrder(models.Model):
         res = super(SaleOrder, self).action_confirm()
         for line in self.order_line:
             if line.employee_id:
-                start_datetime = fields.Datetime.to_string(fields.Datetime.now())[:11] + line.training_date.strftime('%Y-%m-%d')
-                end_datetime = start_datetime + timedelta(hours=8)
+                start_datetime = fields.Datetime.to_string(line.training_date)
+                end_datetime = fields.Datetime.from_string(start_datetime) + timedelta(hours=8)
                 event = self.env['calendar.event'].create({
                     'name': 'Formation - %s' % line.name,
                     'start': start_datetime,
