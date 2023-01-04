ed4f3dede59e1f4c5f27d26061f0b7d76ef31116
NoureddineHelb <nbozanih@helb-prigogine.be>
2023-01-04 19:36:01 +0100

    employee 11

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -5,4 +5,3 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="training date")
     employee_id = fields.Many2one('hr.employee', string="Employee")
-    is_employee = fields.Boolean(string="Is employee", default=True)
