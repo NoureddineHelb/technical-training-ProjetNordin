17c12d8f63a92f15d2ff5f7e1323e9b6f7a98b21
NoureddineHelb <nbozanih@helb-prigogine.be>
2023-01-04 19:24:01 +0100

    correct employee 9

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="training date")
-    employee_id = fields.Many2one('hr.employee', string="Employé", domain=[('is_employee', '=', True)])
+    employee_id = fields.Many2one('hr.employee', string="Employee")
