6a7e385b6fd5962ee150c9dd02e38f384203d7db
NoureddineHelb <80421386+NoureddineHelb@users.noreply.github.com>
2023-01-07 18:03:32 +0100

    Update sale_order.py

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -8,20 +8,15 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     manager_level = fields.Selection(related='partner_id.manager_level')
-    calendar_event_id = fields.Many2one('hr.employee', comodel_name='calendar.event', string='Calendar Event')
 
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
         for line in self.order_line:
             if line.training_date and line.employee_id:
-                employee = line.employee_id
-                event = self.env['calendar.event'].create({
-                    'name': 'Formation Odoo!',
+                self.env['calendar.event'].create({
+                    'name': 'Formation Odoo',
                     'start_date': line.training_date,
                     'stop_date': line.training_date,
-                    'partner_ids': [(4, employee.id)],
-                })
-                employee.write({
-                    'calendar_event_id': event.id,
+                    'partner_id': line.employee_id.partner_id.id,
                 })
         return res
