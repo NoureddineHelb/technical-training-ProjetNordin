4e4c5f2ed91897053d46987e0ed8f7a97f3e83b4
NoureddineHelb <80421386+NoureddineHelb@users.noreply.github.com>
2023-01-07 19:14:55 +0100

    Update sale_order.py

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -12,14 +12,20 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
         for line in self.order_line:
-            if line.employee_id.partner_id:
+            if not line.training_date:
+                raise ValidationError("La date est manquante ou invalide.")
+            start_datetime = fields.Datetime.to_string(line.training_date)
+            end_datetime = fields.Datetime.from_string(start_datetime) + timedelta(hours=8)
+            if line.training_date and line.employee_id:
+                # Créez un partenaire pour l'employé sélectionné
+                partner = self.env['res.partner'].create({'name': line.employee_id.name})
+                # Affectez le partenaire à l'employé sélectionné
+                line.employee_id.partner_id = partner
                 self.env['calendar.event'].create({
                     'name': 'Formation Odoo',
-                    'start_date': line.training_date,
-                    'stop_date': line.training_date,
-                    'partner_id': line.employee_id.partner_id.id,
-                    'attendee_ids': [(4, line.employee_id.partner_id.id)],
+                    'start_date': start_datetime,
+                    'stop_date': end_datetime,
+                    'partner_id': partner.id,
+                    'attendee_ids': [(4, partner.id)],
                 })
-            else:
-                raise ValidationError("L'employé sélectionné n'a pas de partenaire associé.")
         return res
