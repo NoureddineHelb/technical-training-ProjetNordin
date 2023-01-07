ef483a75ee60b6f8f7c07b2de9567a3b65a173b4
NoureddineHelb <80421386+NoureddineHelb@users.noreply.github.com>
2023-01-07 18:49:37 +0100

    assign employee

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -13,18 +13,10 @@ class SaleOrder(models.Model):
         res = super(SaleOrder, self).action_confirm()
         for line in self.order_line:
             if line.training_date and line.employee_id:
-                # Recherchez tous les enregistrements d'employés qui n'ont pas de partenaire affecté
-                employees = self.env['hr.employee'].search([('partner_id', '=', False)])
-
-                # Pour chaque employé, créez un partenaire et affectez-le à l'employé
-                for employee in employees:
-                    partner = self.env['res.partner'].create({'name': employee.name})
-                    employee.partner_id = partner
-                if employee.partner_id:
-                    self.env['calendar.event'].create({
-                        'name': 'Formation Odoo',
-                        'start_date': line.training_date,
-                        'stop_date': line.training_date,
-                        'partner_id': employee.partner_id.id,
-                    })
+                self.env['calendar.event'].create({
+                    'name': 'Formation Odoo',
+                    'start_date': line.training_date,
+                    'stop_date': line.training_date,
+                    'partner_id': line.employee_id.partner_id.id,
+                })
         return res
