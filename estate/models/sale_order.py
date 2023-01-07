df83af9ddcf94879c899ce6833d74c07285e43e7
NoureddineHelb <80421386+NoureddineHelb@users.noreply.github.com>
2023-01-07 17:03:49 +0100

    07-3

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -9,23 +9,20 @@ class SaleOrder(models.Model):
 
     manager_level = fields.Selection(related='partner_id.manager_level')
 
-    class SaleOrder(models.Model):
-        _inherit = 'sale.order'
-
-        # Surcharge de la méthode action_confirm pour créer un événement dans le calendrier de l'employé sélectionné
-        def action_confirm(self):
-            # Appel de la méthode originale
-            res = super(SaleOrder, self).action_confirm()
-            # Parcours de toutes les lignes de commande de la commande de vente
-            for line in self.order_line:
-                # Si une date de formation et un employé sont sélectionnés pour la ligne de commande
-                if line.training_date and line.employee_id:
-                    # Création de l'événement dans le calendrier de l'employé
-                    self.env['calendar.event'].create({
-                        'name': 'Formation Odoo',
-                        'start_date': line.training_date,
-                        'stop_date': line.training_date,
-                        'partner_ids': [(4, line.employee_id.address_home_id.id)],
-                    })
-            # Renvoi du résultat de la méthode originale
-            return res
+    # Surcharge de la méthode action_confirm pour créer un événement dans le calendrier de l'employé sélectionné
+    def action_confirm(self):
+        # Appel de la méthode originale
+        res = super(SaleOrder, self).action_confirm()
+        # Parcours de toutes les lignes de commande de la commande de vente
+        for line in self.order_line:
+            # Si une date de formation et un employé sont sélectionnés pour la ligne de commande
+            if line.training_date and line.employee_id:
+                # Création de l'événement dans le calendrier de l'employé
+                self.env['calendar.event'].create({
+                    'name': 'Formation Odoo',
+                    'start_date': line.training_date,
+                    'stop_date': line.training_date,
+                    'partner_ids': [(4, line.employee_id.address_home_id.id)],
+                })
+        # Renvoi du résultat de la méthode originale
+        return res
