32bb31a0c5d243846ae626e8709ae39d40421724
NoureddineHelb <nbozanih@helb-prigogine.be>
2023-01-08 21:51:01 +0100

    Update res_groups.py

--- estate/models/res_groups.py
+++ estate/models/res_groups.py
@@ -4,7 +4,8 @@ class ResGroups(models.Model):
     _inherit = 'res.groups'
 
     user_type = fields.Selection(
-        [('admin', 'Admin'), ('manager', 'Manager'), ('partner', 'Partner')],
+        [('admin', 'Admin'), ('manager_1', 'Manager Level 1'), ('manager_2', 'Manager Level 2'),
+         ('manager_3', 'Manager Level 3'), ('partner', 'Partner')],
         string='User Type',
         required=True,
         default='partner'
