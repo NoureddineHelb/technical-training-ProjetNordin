9c91af4ba49a93a2cbd1bc3277cb750209ef0fc4
NoureddineHelb <nbozanih@helb-prigogine.be>
2023-01-08 17:44:56 +0100

    back to old version

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -12,24 +12,30 @@ class SaleOrder(models.Model):
     def action_confirm(self):
         res = super(SaleOrder, self).action_confirm()
         for line in self.order_line:
-            if not line.training_date:
-                raise ValidationError("La date est manquante ou invalide.")
-            start_datetime = fields.Datetime.to_string(line.training_date)
-            end_datetime = fields.Datetime.from_string(start_datetime) + timedelta(hours=8)
-            # recherche le partenaire de l'employé sélectionné selon le nom
-            partner = self.env['res.partner'].search([('name', '=', line.employee_id.name)], limit=1)
-            if not partner:
-                raise ValidationError("L'employé sélectionné n'a pas de partenaire ")
             if line.employee_id:
-                employee_id = line.employee_id.id
-                self.env['calendar.event'].create({
-                    'name': 'Formation Odoo',
-                    'start_date': start_datetime,
-                    'stop_date': end_datetime,
-                    'partner_ids': [(4, employee_id)],
-                    'attendee_ids': [(4, employee_id)],
-                })
-            else:
-                raise ValidationError("L'employé sélectionné n'existe pas.")
-
+                if not line.training_date:
+                    raise ValidationError("La date est manquante ou invalide.")
+                start_datetime = fields.Datetime.to_string(line.training_date)
+                end_datetime = fields.Datetime.from_string(start_datetime) + timedelta(hours=8)
+                partner = self.env['res.partner'].search([('name', '=', line.employee_id.name)], limit=1)
+                if not partner:
+                    raise ValidationError("L'employé sélectionné n'a pas de partenaire ")
+                if line.employee_id.user_id:
+                    user_id = line.employee_id.user_id.id
+                else:
+                    user_id = self.env.user.id
+                vals = {
+                    'name': 'Formation - %s' % line.name,
+                    'start': start_datetime,
+                    'stop': end_datetime,
+                    'partner_ids': [(4, line.employee_id.id)],
+                    'privacy': 'confidential',
+                    'user_id': user_id,
+                    'attendee_ids': [(4, line.employee_id.id)],
+                }
+                event = self.env['calendar.event'].create(vals)
+                if not event:
+                    raise ValueError("L'événement n'a pas été créé correctement !")
+                if event.partner_ids != [(4, line.employee_id.id)] != [(4, line.employee_id.id)]:
+                    raise ValueError("L'événement n'a pas été attribué correctement aux participants !")
         return res
