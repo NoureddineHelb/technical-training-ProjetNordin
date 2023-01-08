cb212e54aa8042628d033dde7ba4b003fd8f1ed8
NoureddineHelb <nbozanih@helb-prigogine.be>
2023-01-08 23:31:08 +0100

    Update sale_order.py

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -73,21 +73,20 @@ class SaleOrder(models.Model):
         return res
 
     def request_approval(self):
-        for order in self:
-            for group in self.env.user.groups_id:
-                if group.user_type != 'manager_3' or group.user_type != 'manager_2' or group.user_type != 'manager_1':
-                    manager_1_groups = self.env['res.groups'].search([('user_type', '=', 'manager_1')])
-                    manager_1_users = self.env['res.users'].search([('groups_id', 'in', manager_1_groups.ids)])
-                    if manager_1_users:
-                        # Envoyer l'activité au premier manager_1 trouvé
-                        summary = "Demande d'approbation de la commande %s" % order.name
-                        body = "Une commande de vente a été soumise à votre approbation. Veuillez vérifier les détails de la commande et confirmer ou refuser sa validation. "
-                        self.env['mail.activity'].create({
-                            'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
-                            'note': body,
-                            'summary': summary,
-                            'res_id': order.id,
-                            'res_model_id': self.env.ref('sale.model_sale_order').id,
-                            'user_id': manager_1_users[0].id,
-                        })
-            return True
+        for group in self.env.user.groups_id:
+            if group.user_type != 'manager_3' or group.user_type != 'manager_2' or group.user_type != 'manager_1':
+                manager_1_groups = self.env['res.groups'].search([('user_type', '=', 'manager_1')])
+                manager_1_users = self.env['res.users'].search([('groups_id', 'in', manager_1_groups.ids)])
+                if manager_1_users:
+                    # Envoyer l'activité au premier manager_1 trouvé
+                    summary = "Demande d'approbation de la commande %s" % self.name
+                    body = "Une commande de vente a été soumise à votre approbation. Veuillez vérifier les détails de la commande et confirmer ou refuser sa validation. "
+                    self.env['mail.activity'].create({
+                        'activity_type_id': self.env.ref('mail.mail_activity_data_todo').id,
+                        'note': body,
+                        'summary': summary,
+                        'res_id': self.id,
+                        'res_model_id': self.env.ref('sale.model_sale_order').id,
+                        'user_id': manager_1_users[0].id,
+                    })
+        return True
