00f588e35bab9ce0e26803cd041ed5e0ef7ac29e
NoureddineHelb <nbozanih@helb-prigogine.be>
2023-01-08 16:55:55 +0100

    test

--- estate/models/sale_order.py
+++ estate/models/sale_order.py
@@ -9,25 +9,4 @@ class SaleOrder(models.Model):
 
     manager_level = fields.Selection(related='partner_id.manager_level')
 
-    def action_confirm(self):
-        res = super(SaleOrder, self).action_confirm()
-        for line in self.order_line:
-            if not line.training_date:
-                raise ValidationError("La date est manquante ou invalide.")
-            start_datetime = fields.Datetime.to_string(line.training_date)
-            end_datetime = fields.Datetime.from_string(start_datetime) + timedelta(hours=8)
-            # recherche le partenaire de l'employé sélectionné selon le nom
-            partner = self.env['res.partner'].search([('name', '=', line.employee_id.name)], limit=1)
-            if not partner:
-                raise ValidationError("L'employé sélectionné n'a pas de partenaire ")
-            if line.employee_id:
-                self.env['calendar.event'].create({
-                    'name': 'Formation Odoo',
-                    'start_date': start_datetime,
-                    'stop_date': end_datetime,
-                    'partner_id': partner.id,
-                    'attendee_ids': [(4, partner.id)],
-                })
-            else:
-                raise ValidationError("L'employé sélectionné n'existe pas.")
-        return res
+    
--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -1,7 +1,27 @@
-from odoo import fields, models
+from datetime import timedelta
+
+from odoo.exceptions import ValidationError
+from odoo import api, models, fields
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="training date")
     employee_id = fields.Many2one('hr.employee', string="Employee")
+
+    def create(self, values):
+        line = super(SaleOrderLine, self).create(values)
+        if line.employee_id and line.training_date:
+            start_datetime = fields.Datetime.to_string(line.training_date)
+            end_datetime = fields.Datetime.from_string(start_datetime) + timedelta(hours=8)
+            partner = self.env['res.partner'].search([('name', '=', line.employee_id.name)], limit=1)
+            if not partner:
+                raise ValidationError("L'employé sélectionné n'a pas de partenaire ")
+            self.env['calendar.event'].create({
+                'name': 'Formation Odoo',
+                'start_date': start_datetime,
+                'stop_date': end_datetime,
+                'partner_id': partner.id,
+                'attendee_ids': [(4, partner.id)],
+            })
+        return line
